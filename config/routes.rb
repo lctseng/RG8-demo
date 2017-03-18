4fd683952ce06da702d11c5f2e7ba13c6306a713
Liang-Chi Tseng <lctseng@cs.nctu.edu.tw>
2017-03-18 14:34:09 +0800

    init commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  devise_for :users, controllers: { omniauth_callbacks: "omniauth_callbacks" }
+  resources :products
+  resources :articles
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
